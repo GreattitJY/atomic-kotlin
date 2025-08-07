ca966c5f9ea1b12b9823fb819160614600f4c2fe
GreattitJY <great.tit.jaejae@gmail.com>
2025-08-07 18:05:15 +0900

    add: maps

---
+++ src/maps/ReadOnlyMaps.kt
@@ -0,0 +1,15 @@
+package maps
+
+import atomictest.eq
+
+fun main() {
+    val m = mapOf(5 to "five", 6 to "six")
+    m[5] eq "five"
+
+    m + (4 to "four") // m 그대로
+    m eq mapOf(5 to "five", 6 to "six")
+    val m2 = m + (4 to "four")
+    m2 eq mapOf(5 to "five", 6 to "six", 4 to "four")
+
+
+}
