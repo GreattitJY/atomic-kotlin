36419df44c14723c24b502059dd136c266465582
GreattitJY <great.tit.jaejae@gmail.com>
2025-08-29 10:57:06 +0900

    add: week3 복습중

--- src/main/kotlin/weeks/week3/CCC18S1.kt
+++ src/main/kotlin/weeks/week3/CCC18S1.kt
@@ -2,35 +2,25 @@ package weeks.week3
 
 fun ccc18s1() {
     val input = System.`in`.bufferedReader()
-    val output = System.out.bufferedWriter()
 
-    var N = input.readLine().toIntOrNull() ?: throw Throwable("invalid N")
-    if (N !in 3..100) throw Throwable("out of range 3..100 N: $N")
-
-    val vList = mutableListOf<Int>()
-    while (N > 0) {
-        val v = input.readLine().toIntOrNull() ?: throw Throwable("invalid v")
-        if (v !in -1_000_000_000..1_000_000_000) throw Throwable("out of range -1_000_000_000..1_000_000_000 v: $v")
-        vList.add(v)
-        N--
+    val N = input.readLine().toIntOrNull() ?: throw Throwable("invalid int N")
+    if (N !in 3..100) throw Throwable("out of range 3..100 B: N: $N")
+    val list = mutableListOf<Int>()
+    var i = 0
+    while (i < N) {
+        val V = input.readLine().toIntOrNull() ?: throw Throwable("invalid int V")
+        if (V !in -1_000_000_000..1_000_000_000) throw Throwable("out of range -1_m..1m V: $V")
+        list.add(V)
+        i++
     }
+    list.sort()
 
-    vList.sort()
-    val distList = mutableListOf<Double>()
-    for (i in 0..<vList.size - 1) {
-        val A = vList[i]
-        val B = vList[i + 1]
-        val dist = (B - A) / 2.0
-        distList.add(dist)
+    val size = mutableListOf<Double>()
+    for (i in 1..<list.lastIndex) {
+        size.add(abs(list[i + 1] - list[i - 1]).toDouble() / 2.0)
     }
+    size.sort()
 
-    var minSize = Double.MAX_VALUE
-    for (i in 0..<distList.size - 1) {
-        val units = distList[i] + distList[i + 1]
-        minSize = Math.min(units, minSize)
-    }
-
-    val result = String.format("%.1f", minSize)
-    output.write(result)
+    println("$.1f".format(size[0]))
     output.flush()
 }
