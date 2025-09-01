22fdee907446ef7d65bb401d5babd1303b967682
GreattitJY <great.tit.jaejae@gmail.com>
2025-09-01 16:57:51 +0900

    add: week4 dmopc14c5p1 - 함수 활용(함수명은 연산의 이름)

---
+++ src/main/kotlin/weeks/week4/DMOPC14C5P1.kt
@@ -0,0 +1,18 @@
+package weeks.week4
+
+fun inputInt(v: String, range: IntRange): Int {
+    val result = v.toIntOrNull() ?: throw Throwable("invalid int")
+    if (result !in range) throw Throwable("out of $range: $result")
+    return result
+}
+
+fun dmopc14c5p1() {
+    val input = System.`in`.bufferedReader()
+
+    val r = input.readLine().let { inputInt(it, 1..100) }
+    val h = input.readLine().let { inputInt(it, 1..100) }
+
+    val core = Math.PI * r * r * h / 3
+
+    println(core)
+}
